caaa420fcf7bc32b0fb15596ef035bcaca7e2199
Henrique Gasques <henriquegasques@gmail.com>
2024-02-26 13:44:08 -0300

    [Haskell] List-Ops

---
+++ haskell/list-ops/src/ListOps.hs
@@ -0,0 +1,48 @@
+module ListOps
+  ( length
+  , reverse
+  , map
+  , filter
+  , foldr
+  , foldl'
+  , (++)
+  , concat
+  ) where
+
+import Prelude hiding
+  ( length, reverse, map, filter, foldr, (++), concat )
+
+foldl' :: (b -> a -> b) -> b -> [a] -> b
+foldl' _ acc [] = acc
+foldl' f acc (x:xs) = acc `seq` foldl' f (f acc x) xs
+
+foldr :: (a -> b -> b) -> b -> [a] -> b
+foldr _ acc [] = acc
+foldr f acc (x:xs) = f x (foldr f acc xs)
+
+length :: [a] -> Int
+length [] = 0
+length (_:xs) = 1 + length xs
+
+reverse :: [a] -> [a]
+reverse [] = []
+reverse (x:xs) = reverse xs ++ [x]
+
+map :: (a -> b) -> [a] -> [b]
+map _ [] = []
+map f (x:xs) = f x : map f xs
+
+filter :: (a -> Bool) -> [a] -> [a]
+filter _ [] = []
+filter p (x:xs) = if p x then x : filter p xs else filter p xs
+
+(++) :: [a] -> [a] -> [a]
+[] ++ [] = []
+xs ++ [] = xs
+[] ++ ys = ys
+xs ++ ys = concat [xs, ys]
+
+concat :: [[a]] -> [a]
+concat [] = []
+concat ([]:xs) = concat xs
+concat ((x:xs):xss) = x : concat (xs : xss)
